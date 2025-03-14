3074a3f2f064ce8a4c55b7915cfbcbbdda16eafd
Diogo Braz <dm.braz@campus.fct.unl.pt>
2025-03-14 11:39:44 +0000

    SyNumpy removed

--- app/components/Other.tsx
+++ app/components/Other.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useEffect } from 'react'
-import { SiNumpy, SiOpencv, SiPandas, SiPytorch } from 'react-icons/si'
+import {SiOpencv, SiPandas, SiPytorch } from 'react-icons/si'
 import gsap from 'gsap';
 function Other() {
     useEffect(()=>{
