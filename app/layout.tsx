bd6733b796d70a8b6d4bb094c3a4d88edb294cb6
Diogo Braz <dm.braz@campus.fct.unl.pt>
2025-03-26 11:31:04 +0000

    Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', 'YOUR_TRACKING_ID');
+                gtag('config', 'G-6E85Q1SJKHD');
               `,
             }}
           ></script>
